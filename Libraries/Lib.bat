64c15153563bc7e8603f1462cf212719339fc87e
Mordojo <mordojo09@gmail.com>
2020-01-26 15:45:30 -0500

    Updated pip allure

--- Libraries/Lib.bat
+++ Libraries/Lib.bat
@@ -2,4 +2,4 @@ pip install robotframework
 pip install --upgrade robotframework-seleniumlibrary
 pip install --upgrade RESTinstance
 pip install --upgrade robotframework-jsonlibrary
-pip install allure-robotframework
+pip install allure-robotframework==2.8.7
