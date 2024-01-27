1ec66003fd4d77eb689ef77f3dd11a9dc33674f0
Mordojo <58402803+Mordojo@users.noreply.github.com>
2024-01-27 12:08:43 -0500

    Updated3 Lib.bat

--- Libraries/Lib.bat
+++ Libraries/Lib.bat
@@ -1,5 +1,5 @@
-pip install robotframework 
-pip install --upgrade robotframework-seleniumlibrary
-pip install --upgrade RESTinstance
+pip install robotframework==3.2.2 
+pip install robotframework-seleniumlibrary==5.1.1
+pip install RESTinstance====1.3.0
 pip install --upgrade robotframework-jsonlibrary
 pip install allure-robotframework
