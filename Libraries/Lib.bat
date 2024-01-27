998d53c4f08eaf2d9307bf425bbbdff7e95277d4
Mordojo <58402803+Mordojo@users.noreply.github.com>
2024-01-27 11:55:59 -0500

    Updated2 "Lib.bat"

--- Libraries/Lib.bat
+++ Libraries/Lib.bat
@@ -1,5 +1,4 @@
-pip install robotframework
-pip install selenium==4.9.1 
+pip install robotframework 
 pip install --upgrade robotframework-seleniumlibrary
 pip install --upgrade RESTinstance
 pip install --upgrade robotframework-jsonlibrary
